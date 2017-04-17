04f6f39e89c76590d2b353eb8e6d76360fa18578
ddrdushy <dushy2009@gmail.com>
2017-04-17 11:03:47 +0530

    added attack and special attack functions

--- First Vue.js application/app.js
+++ First Vue.js application/app.js
@@ -12,16 +12,57 @@ new Vue({
 			this.playerHealth = 100;
 		},
 		attack: function(){
+			var damage = this.calculateDamage(3,10);
+			this.monsterHealth -= damage;
 
+			if(this.checkWin()){
+				return;
+			}
+			
+			this.monsterAttack();
+			this.checkWin();
 		},
 		specialAttack: function(){
+			var damage = this.calculateDamage(10,20);
+			this.monsterHealth -= damage;
 
+			if(this.checkWin()){
+				return;
+			}
+
+			this.monsterAttack();
+			this.checkWin();
 		},
 		heal: function(){
 
 		},
 		giveup: function(){
-			
+
+		},
+		monsterAttack: function(){
+			damage = this.calculateDamage(5,12);
+			this.playerHealth -= damage;
+		}
+		calculateDamage: function(min,max){
+			return Math.max(Math.floor(Math.random() * max) + 1, min);
+		},
+		checkWin: function(){
+			if(this.monsterHealth <= 0){
+				if(confirm("You Won! New game?")){
+					this.startGame();
+				}else{
+					this.gameIsRunning = false;
+				}
+				return true;
+			}else if(this.playerHealth <= 0){
+				if(confirm("You Lost! New game?")){
+					this.startGame();
+				}else{
+					this.gameIsRunning = false;
+				}
+				return true;
+			}
+			return false;
 		}
 	}
 });
