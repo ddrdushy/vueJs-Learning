db48df2e283ecd36d08ed2c551cc8292307d53fc
ddrdushy <dushy2009@gmail.com>
2017-04-24 17:01:09 +0530

    communicate with child to parent component

--- Second App/newApp/src/components/User.vue
+++ Second App/newApp/src/components/User.vue
@@ -10,10 +10,14 @@
                 <app-user-detail
                         :name="name"
                         @nameWasReset="name = $event"
-                        :resetFn="resetName"></app-user-detail>
+                        :resetFn="resetName"
+                        :userAge="age"></app-user-detail>
             </div>
             <div class="col-xs-12 col-sm-6">
-                <app-user-edit></app-user-edit>
+                <app-user-edit
+                        :userAge="age"
+                        @ageWasEditted = "age = $event"
+                ></app-user-edit>
             </div>
         </div>
     </div>
@@ -26,7 +30,8 @@
     export default {
         data: function () {
             return {
-                name: 'Max'
+                name: 'Max',
+                age: 27
             };
         },
         components: {
--- Second App/newApp/src/components/UserDetail.vue
+++ Second App/newApp/src/components/UserDetail.vue
@@ -3,6 +3,7 @@
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
         <p>User Name: {{ switchName() }}</p>
+        <p>User Name: {{ userAge }}</p>
         <button @click="resetName">Reset Name</button>
         <button @click="resetFn()">Reset Name</button>
     </div>
@@ -15,7 +16,8 @@
                 type: String,
                 default: 'Don'
             },
-            resetFn: Function
+            resetFn: Function,
+            userAge: Number
         },
         methods: {
             switchName(){
